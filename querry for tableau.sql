481548859310730cf4d6a4b8b1514997fd4b4085
Kestri <115745377+Kestri@users.noreply.github.com>
2022-10-31 14:58:58 +0100

    Add files via upload

---
+++ querry for tableau.sql
@@ -0,0 +1,31 @@
+--Querries i will be using for Tableau project
+--1
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From [Portfolio Project]..covid_deaths
+where continent is not null 
+order by 1,2
+
+--2
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From [Portfolio Project]..covid_deaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+--3
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From [Portfolio Project]..covid_deaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+-- I need to select top 10000 because i cant import more data to excel
+Select top (10000) Location, Population, date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From [Portfolio Project]..covid_deaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
